b04dc7e39e0f2661e44aeefc0b020b3e8113de81
Carlton Bickford <carltonbickford@gmail.com>
2016-02-28 14:04:25 -0700

    change the way tables are created

--- setup.php
+++ setup.php
@@ -10,61 +10,69 @@
 <?php // Example 26-3: setup.php
   require_once 'functions.php';
 
+  $sql =<<<EOF
+    CREATE TABLE IF NOT EXISTS members 
+      (usr varchar(16),
+       pass varchar(16));
+EOF;
+  
+  postgres_query($sql);
+  
+  $sql =<<<EOF
+    CREATE TABLE IF NOT EXISTS messages 
+      (id SERIAL PRIMARY KEY,
+       auth varchar(16),
+       recip varchar(16),
+       pm char(1),
+       time timestamp,
+       message varchar(4096));
+EOF;
+  
+  postgres_query($sql);
+  
+  $sql =<<<EOF
+    CREATE TABLE IF NOT EXISTS friends 
+      (usr varchar(16),
+       friend varchar(16));
+EOF;
+
+  postgres_query($sql);
+  
+  $sql =<<<EOF
+    CREATE TABLE IF NOT EXISTS profiles
+      (usr varchar(16),
+       text varchar(4096));    
+EOF;
+  
+  postgres_query($sql);
+  
+  function postgres_query($sql) {
+    $ret = pg_query($sql);
+    if(!$ret){
+        echo pg_last_error($db);
+    } else {
+      echo "$sql<br>Success.<br>";
+    }
+  }
+  
 //  createTable('members',
-//              'user VARCHAR(16),
-//              pass VARCHAR(16),
-//              INDEX(user(6))');
-
-  createTable('members',
-              'usr varchar(16), pass varchar(16)');
-              
-  //createIndex('members','usr');
-
-  
-//  createTable('messages', 
-//              'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//              auth VARCHAR(16),
-//              recip VARCHAR(16),
-//              pm CHAR(1),
-//              time INT UNSIGNED,
-//              message VARCHAR(4096),
-//              INDEX(auth(6)),
-//              INDEX(recip(6))');
-
-  createTable('messages',
-              'id SERIAL PRIMARY KEY,
-              auth varchar(16),
-              recip varchar(16),
-              pm char(1),
-              time timestamp,
-              message varchar(4096)');
-  
-  //createIndex('messages', 'auth');
-  //createIndex('messages', 'recip');
-  
+//              'usr varchar(16), pass varchar(16)');
+//              
+//  createTable('messages',
+//              'id SERIAL PRIMARY KEY,
+//              auth varchar(16),
+//              recip varchar(16),
+//              pm char(1),
+//              time timestamp,
+//              message varchar(4096)');
+//  
 //  createTable('friends',
-//              'user VARCHAR(16),
-//              friend VARCHAR(16),
-//              INDEX(user(6)),
-//              INDEX(friend(6))');
-
-  createTable('friends',
-              'usr varchar(16),
-              friend varchar(16)');
-  
-  //createIndex('friends', 'usr');
-  //createIndex('friends', 'friend');
-  
+//              'usr varchar(16),
+//              friend varchar(16)');
+//  
 //  createTable('profiles',
-//              'user VARCHAR(16),
-//              text VARCHAR(4096),
-//              INDEX(user(6))');
-  
-  createTable('profiles',
-              'usr varchar(16),
-              text varchar(4096)');
-  
-  //createIndex('profiles', 'usr');
+//              'usr varchar(16),
+//              text varchar(4096)');
 
 ?>
 

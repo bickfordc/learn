95af021c65e3261b435315adb22ba7dfb1f1c268
Carlton Bickford <carltonbickford@gmail.com>
2016-04-09 18:58:18 -0600

    initial db load

--- loadDb.php
+++ loadDb.php
@@ -14,7 +14,76 @@
         }
             
         move_uploaded_file($_FILES['filename']['tmp_name'], $name);    
-        echo "Uploaded image '$name'<br><img src='$name'>";  
+        //echo "Uploaded image '$name'<br><img src='$name'>"; 
+        
+        $file = fopen($name, "r");
+        if ($file == NULL)
+        {
+            die("Could not open file $file");
+        }
+ 
+        $firstLine = true;
+        $lastAcctMarker = -1;
+        
+        while (!feof($file))
+        {
+            $row = fgetcsv($file, 300, ",");
+            if ($firstLine)
+            {
+                $firstLine = false;
+                continue;
+            }
+              
+            $cardNumber = $row[1];
+            if ($cardNumber == NULL)
+            {
+                continue;
+            }
+            
+            $cardHolder = $row[0];
+            $isSold =  $row[2] == 'y' ? 't' : 'f';
+
+            queryPostgres("INSERT INTO cards (id, sold, card_holder) VALUES ($1, $2, $3)",
+                    array($cardNumber, $isSold, $cardHolder));
+            
+            if ($isSold == 't')
+            {
+                $firstName  = $row[3];
+                $lastName   = $row[4];
+                if ($lastName == NULL)
+                {
+                    continue;
+                }
+                
+                $isStudentActive = $row[6] == 'y' ? 't' : 'f';
+                
+                $acctMarker = $row[5];
+                if ($lastAcctMarker != $acctMarker) 
+                {
+                    $newAccount = true;
+                    $lastAcctMarker = $acctMarker;
+                    
+                    queryPostgres("INSERT INTO students (first, last, active) VALUES ($1, $2, $3)",
+                        array($firstName, $lastName, $isStudentActive));
+                }
+                
+                // get the auto incremented id from the student account 
+                $result = queryPostgres("SELECT id FROM students WHERE first=$1 and last=$2",
+                        array($firstName, $lastName));
+                
+                if (pg_num_rows($result) > 0) 
+                {
+                    $theRow = pg_fetch_array($result);
+                    $studentId = $theRow['id'];
+                }
+                
+                queryPostgres("INSERT INTO student_cards (student, card) VALUES ($1, $2)",
+                        array($studentId, $cardNumber));
+            }
+
+        }
+        
+        fclose($file);
         
     } 
     
